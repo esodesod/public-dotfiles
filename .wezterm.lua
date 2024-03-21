b3a79680787f3f30b8e18921c1493a88a6d97cfe
Espen Ødegaard <esodesod@users.noreply.github.com>
2024-03-21 23:30:28 +0100

    Create .wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,17 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = 'AdventureTime'
+
+-- fonts
+config.font = wezterm.font 'ComicShannsMono Nerd Font'
+config.font_size = 16.0
+
+-- and finally, return the configuration to wezterm
+return config
