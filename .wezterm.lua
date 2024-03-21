09d4c013be03ae04f165bbb9cdf46badea62bb6f
Espen Ødegaard <esodesod@users.noreply.github.com>
2024-03-21 23:32:21 +0100

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,6 +12,7 @@ local config = wezterm.config_builder()
 -- fonts
 config.font = wezterm.font 'ComicShannsMono Nerd Font'
 config.font_size = 16.0
+config.hide_tab_bar_if_only_one_tab = true
 
 -- and finally, return the configuration to wezterm
 return config
