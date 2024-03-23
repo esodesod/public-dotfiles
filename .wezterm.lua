b80f7a1b1e46a017715677b6d314f40f54645e64
Espen Ødegaard <esodesod@users.noreply.github.com>
2024-03-23 19:08:49 +0100

    fix macos left option key not working as expected
    E.g. to enter `{` and/or `[` in macOS

--- .wezterm.lua
+++ .wezterm.lua
@@ -14,23 +14,9 @@ config.font = wezterm.font 'ComicShannsMono Nerd Font'
 config.font_size = 16.0
 config.hide_tab_bar_if_only_one_tab = true
 
--- test disabling some conflicting keys
-config.keys = {
-  -- Turn off the default CMD-m Hide action, allowing CMD-m to
-  -- be potentially recognized and handled by the tab
-  {
-    key = '[',
-    mods = 'SHIFT|SUPER',
-    action = wezterm.action.DisableDefaultAssignment,
-  },
-}
-
--- test disable all keymaps
-config.disable_default_key_bindings = true
-config.keys = {
-  -- CTRL-SHIFT-l activates the debug overlay
-  { key = 'L', mods = 'CTRL', action = wezterm.action.ShowDebugOverlay },
-}
+-- macOS left and right option keys.
+-- see https://wezfurlong.org/wezterm/config/keyboard-concepts.html#microsoft-windows-and-ctrl-alt-altgr
+config.send_composed_key_when_left_alt_is_pressed = true
 
 -- and finally, return the configuration to wezterm
 return config
