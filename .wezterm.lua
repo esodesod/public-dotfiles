67c73a2c8320087acf180029eba2d6a7f5552210
Espen Ødegaard <esodesod@users.noreply.github.com>
2024-03-23 18:56:37 +0100

    test disable default key bindings

--- .wezterm.lua
+++ .wezterm.lua
@@ -14,5 +14,23 @@ config.font = wezterm.font 'ComicShannsMono Nerd Font'
 config.font_size = 16.0
 config.hide_tab_bar_if_only_one_tab = true
 
+-- test disabling some conflicting keys
+config.keys = {
+  -- Turn off the default CMD-m Hide action, allowing CMD-m to
+  -- be potentially recognized and handled by the tab
+  {
+    key = '[',
+    mods = 'SHIFT|SUPER',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+}
+
+-- test disable all keymaps
+config.disable_default_key_bindings = true
+config.keys = {
+  -- CTRL-SHIFT-l activates the debug overlay
+  { key = 'L', mods = 'CTRL', action = wezterm.action.ShowDebugOverlay },
+}
+
 -- and finally, return the configuration to wezterm
 return config
