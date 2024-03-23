8b7f909a2c2a8cb4606458b5a899cc240b37b39f
Espen Ødegaard <esodesod@users.noreply.github.com>
2024-03-23 21:39:07 +0100

    trying to get file:// links to work, as in iterm2

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- color scheme:
- config.color_scheme = 'Gruvbox Dark (Gogh)'
+config.color_scheme = 'Gruvbox Dark (Gogh)'
 
 -- fonts
 config.font = wezterm.font 'ComicShannsMono Nerd Font'
@@ -17,6 +17,16 @@ config.hide_tab_bar_if_only_one_tab = true
 -- in macOS, make left option key perform composition (AltGr)
 -- see https://wezfurlong.org/wezterm/config/keyboard-concepts.html#microsoft-windows-and-ctrl-alt-altgr
 config.send_composed_key_when_left_alt_is_pressed = true
+
+-- Use the defaults as a base
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+-- add file:// URIs into hyperlink_rules
+table.insert(config.hyperlink_rules, {
+	-- match file:// URIs.
+	regex = "\\bfile://\\S*\\b",
+	format = "$0",
+})
 
 -- return the configuration to wezterm
 return config
