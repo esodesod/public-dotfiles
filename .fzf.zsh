5d8e8de021f1cd1bde549d83abb5072add4e5c19
Espen Ødegaard <esodesod@users.noreply.github.com>
2024-04-07 17:09:54 +0200

    fzf scroll results and scroll preview

--- .fzf.zsh
+++ .fzf.zsh
@@ -15,6 +15,6 @@ source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 # default ctrl-t options
 FZF_CTRL_T_COMMAND="fd --hidden --follow --exclude .git"
 # use fzf-preview.sh
-FZF_CTRL_T_OPTS="--preview='fzf-preview.sh {}' --height=100% --layout=default"
+FZF_CTRL_T_OPTS="--bind='ctrl-k:page-up' --bind='ctrl-j:page-down' --bind='ctrl-u:preview-half-page-up' --bind='ctrl-d:preview-half-page-down' --preview='fzf-preview.sh {}' --height=100% --layout=default"
 
 FZF_DEFAULT_COMMAND='fd --hidden --follow --exclude .git'
